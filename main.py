914170f5b798129878a18a2b8f2fe534c9f74569
Andreas Becker <andreas-becker-98@users.noreply.github.com>
2025-01-04 01:14:24 +0000

    Fix main.py to only run if the main file

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ def get_character_frequency(string: str) -> dict[str, int]:
 
     return dict(sorted(frequency.items(), reverse=True, key=lambda item: item[1]))
 
-
-path_to_book = "books/frankenstein.txt"
-
-with open(path_to_book) as f:
-    file_contents = f.read()
-
-print(f"--- Begin report of {path_to_book} ---")
-print(f"{count_words(file_contents)} words found in the document")
-
-character_frequency = get_character_frequency(file_contents)
-for char in character_frequency:
-    print(f"The '{char}' character was found {character_frequency[char]} times")
-
-print(f"--- End report ---")
+if __name__ == "__main__":
+    path_to_book = "books/frankenstein.txt"
+    
+    with open(path_to_book) as f:
+        file_contents = f.read()
+    
+    print(f"--- Begin report of {path_to_book} ---")
+    print(f"{count_words(file_contents)} words found in the document")
+    
+    character_frequency = get_character_frequency(file_contents)
+    for char in character_frequency:
+        print(f"The '{char}' character was found {character_frequency[char]} times")
+    
+    print(f"--- End report ---")
